7779d4aaa52e09af9438ea85c1ad915f7eba495e
Forthfireboy <forthfireboy@gmail.com>
2024-11-21 12:44:30 +0700

    cnc

---
+++ example_mods/stages/forest.lua
@@ -0,0 +1,6 @@
+function onCreate()
+makeLuaSprite('Fortnite_battle_pass', 'bgs/regular show/bg', -3075,-360)
+scaleObject('Fortnite_battle_pass', 2.6,2.6)
+addLuaSprite('Fortnite_battle_pass', false)
+close(true)
+end
